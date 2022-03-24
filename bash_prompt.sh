6b220865d360b116849ceb10f02234ed3dd6efa6
Michal Bernacki <michal.bernacki121@gmail.com>
2022-03-24 22:26:01 +0100

    fix on PS1 and new file

--- bash_prompt.sh
+++ bash_prompt.sh
@@ -13,5 +13,4 @@ function changes_in_branch {
     fi
 }
 
-
-PS1='\n╭─ \[\e[\033[01;34m\]\u@\h \[\e[38;5;211m\]\w $(changes_in_branch)\n╰▶ \$ '
+PS1='\n\[\033[00m\]╭─ \[\e[\033[01;34m\]\u@\h \[\e[38;5;211m\]\w $(changes_in_branch)\n\[\033[00m\]╰▶ \$ '
