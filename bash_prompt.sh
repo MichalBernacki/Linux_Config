677ef8d96b019bd464568cb44f730c4d1e7b003b
Michal Bernacki <michal.bernacki121@gmail.com>
2022-03-24 21:56:44 +0100

    prompt ready

--- bash_prompt.sh
+++ bash_prompt.sh
@@ -7,9 +7,11 @@ function parse_git_branch {
 function changes_in_branch { 
     if [ -d .git ]; then
 	if expr length + "$(git status -s)" 2>&1 > /dev/null; then     
-	    echo -ne "\033[0;31m$(parse_git_branch)\033[0m"; 
+	    echo -ne "\033[0;31m($(parse_git_branch))\033[0m"; 
 	else
-	    echo -ne "\033[0;32m$(parse_git_branch)\033[0m"; fi; 
+	    echo -ne "\033[0;32m($(parse_git_branch))\033[0m"; fi; 
     fi
 }
 
+
+PS1='\n╭─ \[\e[\033[01;34m\]\u@\h \[\e[38;5;211m\]\w $(changes_in_branch)\n╰▶ \$ '
