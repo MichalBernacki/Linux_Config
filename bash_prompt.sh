066b9c0c3d5c5c9cd747b6d3d4a49e36072c5fdf
Michal Bernacki <michal.bernacki121@gmail.com>
2022-04-03 16:18:27 +0200

    fixes in bash prompt - after this git status is printed in whole git repo

--- bash_prompt.sh
+++ bash_prompt.sh
@@ -4,12 +4,12 @@ function parse_git_branch {
   git branch --no-color 2> /dev/null |  grep '^*' | colrm 1 2
 }
 
-function changes_in_branch { 
-    if [ -d .git ]; then
-	if expr length + "$(git status -s)" 2>&1 > /dev/null; then     
-	    echo -ne "\033[0;31m($(parse_git_branch))\033[0m"; 
-	else
-	    echo -ne "\033[0;32m($(parse_git_branch))\033[0m"; fi; 
+function changes_in_branch {
+    if  [[ "$(git status 2> /dev/null)" ]]; then
+	    if expr length + "$(git status -s 2> /dev/null)" 2>&1 > /dev/null; then     
+	        echo -ne "\033[0;31m($(parse_git_branch))\033[0m"; 
+	    else
+	        echo -ne "\033[0;32m($(parse_git_branch))\033[0m"; fi;
     fi
 }
 
