fa88efdfabe5823b27310bb81e4bdc44a8d457a8
Michal Bernacki <michal.bernacki121@gmail.com>
2023-10-25 20:55:27 +0200

    new changes

--- install_config.sh
+++ install_config.sh
@@ -1,16 +1,26 @@
 #!/bin/bash
 
 function install_nvim_conf {
-
+	echo "todo?"
 }
 
 function install_bashrc {
+	echo "todo?"
+}
 
+function install_dwm {
+	echo "todo?"
+}
+
+function install_progs {
+	echo "todo?"
 }
 
 function run {
 	install_nvim_conf
-	install_bashr
+	install_bashrc
+	install_dwm
+	install_progs
 }
 
 run
