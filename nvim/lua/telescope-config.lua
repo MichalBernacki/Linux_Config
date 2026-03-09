0c089c49616b0c94f5968e6811843f3a9ae279bc
MichalBernacki <michal.bernacki121@gmail.com>
2026-03-09 19:04:41 +0100

    add fw to telescope

--- nvim/lua/telescope-config.lua
+++ nvim/lua/telescope-config.lua
@@ -1,55 +1,37 @@
-local builtin = require('telescope.builtin')
-local actions = require('telescope.actions')
+local set = vim.opt
 
-local function select_one_or_multi(prompt_bufnr)
-    local picker = require('telescope.actions.state').get_current_picker(prompt_bufnr)
-    local multi = picker:get_multi_selection()
-    if not vim.tbl_isempty(multi) then
-        actions.close(prompt_bufnr)
-        for _, j in pairs(multi) do
-            if j.path ~= nil then
-                vim.cmd(string.format('%s %s', 'edit', j.path))
-            end
-        end
-    else
-        actions.select_default(prompt_bufnr)
-    end
+vim.g.mapleader = " "
+vim.g.maplocalleader = " "
+
+local Plug = vim.fn['plug#']
+
+vim.call('plug#begin', '~/.config/nvim/plugged')
+Plug 'folke/tokyonight.nvim'
+Plug 'nvim-lua/plenary.nvim'
+Plug('nvim-telescope/telescope.nvim', { tag = '0.1.8' })
+
+vim.call('plug#end')
+
+vim.cmd "colorscheme tokyonight-night"
+
+local ok, err = pcall(require, "telescope-config")
+if not ok then
+    vim.notify("telescope-config not loaded: " .. err, vim.log.levels.WARN)
 end
 
-require('telescope').setup({
-    defaults = {
-        vimgrep_arguments = {
-            'rg',
-            '--color=never',
-            '--no-heading',
-            '--with-filename',
-            '--line-number',
-            '--column',
-            '--smart-case',
-            '--hidden',
-            '--glob=!.git/',
-        },
-        mappings = {
-            i = {
-                ['<CR>']  = select_one_or_multi,
-                ['<C-j>'] = actions.cycle_history_next,
-                ['<C-k>'] = actions.cycle_history_prev,
-                ['<C-w>'] = actions.send_selected_to_qflist + actions.open_qflist,
-                ['<C-s>'] = actions.preview_scrolling_up,
-                ['<C-a>'] = actions.preview_scrolling_down,
-            },
-        },
-    },
-})
+set.relativenumber = true
+set.termguicolors = true
 
-vim.keymap.set('n', '<leader>ff', builtin.find_files, { desc = 'Find files' })
-vim.keymap.set('n', '<leader>fg', builtin.live_grep, { desc = 'Live grep' })
-vim.keymap.set('n', '<leader>fb', builtin.buffers, { desc = 'Find buffers' })
-vim.keymap.set('n', '<leader>gc', builtin.git_commits, { desc = 'Git commits' })
-vim.keymap.set('n', '<leader>gb', builtin.git_branches, { desc = 'Git branches' })
-vim.keymap.set('n', '<leader>/', function()
-    builtin.current_buffer_fuzzy_find(require('telescope.themes').get_dropdown({
-        winblend = 10,
-        previewer = false,
-    }))
-end, { desc = 'Fuzzy search in buffer' })
+set.mouse = "a"
+set.nu = true
+set.errorbells = false
+
+set.tabstop = 4
+set.shiftwidth = 4
+set.expandtab = true
+set.softtabstop = 4
+
+set.hlsearch = false
+set.incsearch = true
+set.ignorecase = true
+set.smartcase = true
